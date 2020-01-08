9f94129a53a492f3fdba4f48d96058de4f62163d
Tal Mirmelshtein <tal.mirmelshtein@gigya-inc.com>
2020-01-08 13:55:47 +0200

    #106138 - Re-Importing native module implementations.

---
+++ .ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/i501740/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/i501740/Developer/Flutter/gigya_native_screensets_engine"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
