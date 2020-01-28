fffb4651e3134e7e1eede22e1bce5ac34a1f38b8
Sagi <sagi.shmuel@gigya-inc.com>
2020-01-28 15:07:39 +0200

    finish tests for LayoutRender

--- test/rendering_test.dart
+++ test/rendering_test.dart
@@ -19,5 +19,47 @@ void main() {
 
       expect(id1TextFinder, findsOneWidget);
     });
+
+    testWidgets("render without screen", (WidgetTester tester) async {
+      Map<String, Screen> mockData = {};
+      mockData["test"] = Screen("test", NSSAlignment.vertical, []);
+
+      await tester.pumpWidget(MaterialApp(home: NSSLayoutBuilder("").render(mockData)));
+
+      await tester.pump(Duration(seconds: 2), EnginePhase.build);
+
+      final id1TextFinder = find.text('Initial route missmatch.'
+          '\nMarkup does not contain requested route.');
+
+      expect(id1TextFinder, findsOneWidget);
+    });
+
+    testWidgets("render without chidren", (WidgetTester tester) async {
+      Map<String, Screen> mockData = {};
+      mockData["test"] = Screen("test", NSSAlignment.vertical, []);
+
+      await tester.pumpWidget(MaterialApp(home: NSSLayoutBuilder("test").render(mockData)));
+
+      await tester.pump(Duration(seconds: 2), EnginePhase.build);
+
+      final id1TextFinder = find.text('Screen must contain children.'
+          '\nMarkup tag \"screen\" must contain children.');
+
+      expect(id1TextFinder, findsOneWidget);
+    });
+
+    testWidgets("test AppBar widget", (WidgetTester tester) async {
+      Map<String, Screen> mockData = {};
+      mockData['test'] = Screen('test', NSSAlignment.vertical, [NSSWidget(WidgetType.label, 'test label')]);
+      mockData['test'].appBar = {'textKey': 'Test AppBar'};
+
+      await tester.pumpWidget(MaterialApp(home: NSSLayoutBuilder('test').render(mockData)));
+
+      await tester.pump(Duration(seconds: 2), EnginePhase.build);
+
+      final id1TextFinder = find.text('Test AppBar');
+
+      expect(id1TextFinder, findsOneWidget);
+    });
   });
 }
