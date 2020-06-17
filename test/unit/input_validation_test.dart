a10aebc1de359b2b80fa3f98bdebb1c311a79a30
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2020-06-17 13:02:41 +0300

    #107106 - Added additional regex test (dynamic mock).

--- test/unit/input_validation_test.dart
+++ test/unit/input_validation_test.dart
@@ -6,7 +6,7 @@ void main() {
     /// REGEX:
     /// Contains only alpha numerical characters.
     test(
-      'regex test #1',
+      'regex test #1 - various',
       () async {
         var validatorMap = {
           "enabled": true,
@@ -33,7 +33,7 @@ void main() {
       /// (?=.*?[0-9])             // Should contain at least one digit.
       /// (?=.*?[!@%#\$&*~])       // Should contain at least one Special character.
       /// .{8,}                    // Should be at least 8 digits long.
-      'regex test #2',
+      'regex test #2 - password',
       () async {
         var validatorMap = {
           "enabled": true,
@@ -51,6 +51,29 @@ void main() {
         // Invalid formats.
         expect(regExp.hasMatch("12_34"), false);
         expect(regExp.hasMatch("%sdslk&"), false);
+      },
+    );
+
+
+    test(
+      'regex test #3 - email',
+          () async {
+        var validatorMap = {
+          "enabled": true,
+          "value": "^[a-zA-Z0-9.a-zA-Z0-9.!#\$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+          "errorKey": "Invalid format",
+        };
+
+        NssInputValidator regexValidator = NssInputValidator.from(validatorMap);
+        final RegExp regExp = RegExp(regexValidator.value);
+
+        // Valid formats.
+        expect(regExp.hasMatch("abx@cc.com"), true);
+        expect(regExp.hasMatch("someemail@gmail.com"), true);
+
+        // Invalid formats.
+        expect(regExp.hasMatch("aa.com"), false);
+        expect(regExp.hasMatch("aa@bb"), false);
       },
     );
   });
