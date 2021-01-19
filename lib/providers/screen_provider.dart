633b430c8c415eec38121916ea33c09cb44bc1d7
Sagi <sagi.shmuel@sap.com>
2021-01-19 14:08:23 +0200

    fix bug with errors in web mode.

--- lib/providers/screen_provider.dart
+++ lib/providers/screen_provider.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gigya_native_screensets_engine/config.dart';
 import 'package:gigya_native_screensets_engine/ioc/injector.dart';
+import 'package:gigya_native_screensets_engine/models/api.dart';
 import 'package:gigya_native_screensets_engine/models/markup.dart';
 import 'package:gigya_native_screensets_engine/models/screen.dart';
 import 'package:gigya_native_screensets_engine/utils/debug.dart';
