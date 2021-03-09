24d919d4cf72058faf19fbcf0ac46f4d15334f30
Sagi <sagi.shmuel@sap.com>
2021-03-09 14:43:40 +0200

    fix small bug when dir is not exists (CI)

--- nss-schema/env.ts
+++ nss-schema/env.ts
@@ -8,6 +8,11 @@ export const exampleDevFile = path.join(outputDir, 'develop/example.json');
 const fs = require('fs');
 export function getJsonFiles(dir: string, excludeFiles: string[] = []) {
     console.log('~~~', path.join(__dirname, dir));
+
+    if (!fs.existsSync(examplesDir)){
+        fs.mkdirSync(examplesDir);
+    }
+
     return fs.readdirSync(path.join(__dirname, dir))
         .filter(file => file.match(/.*\.json$/) && !excludeFiles.find(ex => file.startsWith(ex)))
         .map(file => path.join(dir,file));
