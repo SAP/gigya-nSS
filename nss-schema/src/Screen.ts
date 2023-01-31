f44a46334be1f1501f79fce7676ef67e7586c6e2
Gal Cohen <gal.cohen@sap.com>
2023-01-31 19:21:22 +0200

    #729 confirm password screen action added

--- nss-schema/src/Screen.ts
+++ nss-schema/src/Screen.ts
@@ -20,7 +20,7 @@ export default new SchemaEntity('Screen', mergeDeep({},
                 'register',
                 'setAccount',
                 'forgotPassword',
-                'confirmPassword',
+                'changePassword',
                 'linkAccount',
                 'otp',
                 'none'
