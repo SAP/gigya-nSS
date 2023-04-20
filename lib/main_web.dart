5b3a1f2c9072b92cb0d7c1cfd50e09bf291356a3
Gal Cohen <gal.cohen@sap.com>
2023-04-20 07:54:52 +0300

    #8537 fix of main web + mock mobile

--- lib/main_web.dart
+++ lib/main_web.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:gigya_native_screensets_engine/ioc/injector.dart';
 import 'package:gigya_native_screensets_engine/ioc/ioc_web.dart';
 import 'package:gigya_native_screensets_engine/widgets/router.dart';
+import 'comm/web_channel.dart';
+import 'config.dart';
+import 'models/markup.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,8 +15,38 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     color: Colors.white,
+  //     initialRoute: '/',
+  //     onGenerateRoute: NssIoc().use(MaterialRouter).generateRoute,
+  //   );
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    WebContainer().startEngine();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
+    return FutureBuilder (
+        future: fetchMarkupAndSchema(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done)
+            return createApp();
+          else
+            return Container();
+        }
+    );}
+
+
+  Widget createApp() {
     return MaterialApp(
+      // showSemanticsDebugger: true,
       debugShowCheckedModeBanner: false,
       color: Colors.white,
       initialRoute: '/',
@@ -21,9 +54,41 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  @override
-  void initState() {
-    super.initState();
-    WebContainer().startEngine();
+  Future<void> fetchMarkupAndSchema() async {
+    final NssConfig config = NssIoc().use(NssConfig);
+    var fetchData = await _markupFromChannel(config!.version);
+    final Markup markup = Markup.fromJson(fetchData.cast<String, dynamic>());
+    config!.markup = markup;
+    // config!.isPlatformAware = markup.platformAware ?? false;
+
+    // Fetch and parse the schema if required in markup preference (and not in mock mode).
+    // if (markup.useSchemaValidations! && !config!.isMock!) {
+    //   engineLogger!.d(
+    //       "startup widget: requesting schema (schemaValidations)",
+    //       tag: Logger.dTag);
+    //   var rawSchema = await widget.channels!.ignitionChannel
+    //       .invokeMethod<Map<dynamic, dynamic>>('load_schema');
+    //   var newSchema = {
+    //     'profile': rawSchema['profileSchema']['fields'],
+    //     'data': rawSchema['dataSchema']['fields'],
+    //     'subscriptions': rawSchema['subscriptionsSchema']['fields'],
+    //     'preferences': rawSchema['preferencesSchema']['fields']
+    //   };
+    //   config!.schema = newSchema;
+    // }
+
+    // Add default localization values that are needed (can be overridden by client).
+    //ErrorUtils().addDefaultStringValues(config!.markup!.localization!);
   }
+
+
+/// Fetch markup from the running platform.
+  Future<Map<dynamic, dynamic>> _markupFromChannel(version) async {
+    final NssWebMethodChannel ignitionChannel = NssIoc().use(NssChannels).ignitionChannel;
+
+    return ignitionChannel.invokeMethod<Map<dynamic, dynamic>>(
+        'ignition', {'version': version});
+  }
+
+
 }
--- lib/mock_mobile.dart
+++ lib/mock_mobile.dart
@@ -1,19 +1,50 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gigya_native_screensets_engine/ioc/injector.dart';
 import 'package:gigya_native_screensets_engine/ioc/ioc_mobile.dart';
+import 'package:gigya_native_screensets_engine/utils/assets.dart';
+import 'package:gigya_native_screensets_engine/utils/error.dart';
+import 'package:gigya_native_screensets_engine/utils/logging.dart';
 import 'package:gigya_native_screensets_engine/widgets/router.dart';
+
+import 'config.dart';
+import 'models/markup.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  // late final NssConfig? config;
+  // late final NssChannels? channels;
+
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    MobileContainer().startEngine(asMock: true);
+  }
+
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder (
+      future: fetchMarkupAndSchema(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done)
+          return createApp();
+        else
+          return Container();
+      }
+    );}
+
+
+  Widget createApp() {
     return MaterialApp(
       // showSemanticsDebugger: true,
       debugShowCheckedModeBanner: false,
@@ -23,9 +54,45 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  @override
-  void initState() {
-    super.initState();
-    MobileContainer().startEngine(asMock: true);
+  Future<void> fetchMarkupAndSchema() async {
+    final NssConfig config = NssIoc().use(NssConfig);
+    var fetchData = await _markupFromMock();
+    final Markup markup = Markup.fromJson(fetchData.cast<String, dynamic>());
+    config!.markup = markup;
+    config!.isPlatformAware = markup.platformAware ?? false;
+
+    // Fetch and parse the schema if required in markup preference (and not in mock mode).
+    // if (markup.useSchemaValidations! && !config!.isMock!) {
+    //   engineLogger!.d(
+    //       "startup widget: requesting schema (schemaValidations)",
+    //       tag: Logger.dTag);
+    //   var rawSchema = await widget.channels!.ignitionChannel
+    //       .invokeMethod<Map<dynamic, dynamic>>('load_schema');
+    //   var newSchema = {
+    //     'profile': rawSchema['profileSchema']['fields'],
+    //     'data': rawSchema['dataSchema']['fields'],
+    //     'subscriptions': rawSchema['subscriptionsSchema']['fields'],
+    //     'preferences': rawSchema['preferencesSchema']['fields']
+    //   };
+    //   config!.schema = newSchema;
+    // }
+
+    // Add default localization values that are needed (can be overridden by client).
+    ErrorUtils().addDefaultStringValues(config!.markup!.localization!);
   }
+  /// Fetch markup from example JSON asset.
+  /// This is used for development & testing.
+  Future<Map<dynamic, dynamic>> _markupFromMock() async {
+    final String json = await AssetUtils.jsonFromAssets('assets/example.json');
+    return jsonDecode(json);
+  }
+
+  /// Fetch markup from the running platform.
+//   Future<Map<dynamic, dynamic>> _markupFromChannel() async {
+// //    var channel = widget.channels.ignitionChannel as NssWebMethodChannel;
+//     return widget.channels!.ignitionChannel.invokeMethod<Map<dynamic, dynamic>>(
+//         'ignition', {'version': widget.config!.version});
+//   }
+
+
 }
