ee20220d02a47990c21fe7674582e1cd2f53f4a7
Gal Cohen <gal.cohen@sap.com>
2023-04-20 11:42:23 +0300

    #8537 some checks

--- lib/main_web.dart
+++ lib/main_web.dart
@@ -35,7 +35,6 @@ class _MyAppState extends State<MyApp> {
 
   Widget createApp() {
     return MaterialApp(
-      // showSemanticsDebugger: true,
       debugShowCheckedModeBanner: false,
       color: Colors.white,
       initialRoute: '/',
