c19048a844c2aac95770e74c9485b104825f27b6
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2023-05-29 12:32:28 +0300

    Version update 1.7.1

--- lib/config.dart
+++ lib/config.dart
@@ -2,7 +2,7 @@ import 'package:gigya_native_screensets_engine/comm/moblie_channel.dart';
 import 'package:gigya_native_screensets_engine/models/markup.dart';
 
 class NssConfig {
-  String version = '1.7.0';
+  String version = '1.7.1';
   bool? isMock = false;
   bool? isPlatformAware = false;
   Markup? markup;
